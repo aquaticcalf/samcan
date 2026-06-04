06d7611a242f5a839e0b004f9a866b9d9a01ea4e
sam <aquaticcalf@proton.me>
2026-06-04 12:58:12 +0530

    coordinates

--- math/rectangle.zig
+++ math/rectangle.zig
@@ -23,6 +23,22 @@ pub fn fromCenter(center: vector2.Vector2, size: vector2.Vector2, out: Rectangle
     out.size = size;
 }
 
+pub fn topleft(r: Rectangle) vector2.Vector2 {
+    return .{ r.position[0], r.position[1] };
+}
+
+pub fn topright(r: Rectangle) vector2.Vector2 {
+    return .{ r.position[0] + r.size[0], r.position[1] };
+}
+
+pub fn bottomright(r: Rectangle) vector2.Vector2 {
+    return .{ r.position[0] + r.size[0], r.position[1] + r.size[1] };
+}
+
+pub fn bottomleft(r: Rectangle) vector2.Vector2 {
+    return .{ r.position[0], r.position[1] + r.size[1] };
+}
+
 test "create" {
     const r = create(1, 2, 3, 4);
     try std.testing.expectEqual(@as(f32, 1), r.position[0]);
@@ -55,4 +71,24 @@ test "fromCenter" {
     try std.testing.expectEqual(@as(f32, 17), r.position[1]);
     try std.testing.expectEqual(@as(f32, 4), r.size[0]);
     try std.testing.expectEqual(@as(f32, 6), r.size[1]);
+}
+
+test "coordinates" {
+    const r = create(1, 2, 3, 4);
+
+    const tl = topleft(r);
+    try std.testing.expectEqual(@as(f32, 1), tl[0]);
+    try std.testing.expectEqual(@as(f32, 2), tl[1]);
+
+    const tr = topright(r);
+    try std.testing.expectEqual(@as(f32, 4), tr[0]);
+    try std.testing.expectEqual(@as(f32, 2), tr[1]);
+
+    const br = bottomright(r);
+    try std.testing.expectEqual(@as(f32, 4), br[0]);
+    try std.testing.expectEqual(@as(f32, 6), br[1]);
+
+    const bl = bottomleft(r);
+    try std.testing.expectEqual(@as(f32, 1), bl[0]);
+    try std.testing.expectEqual(@as(f32, 6), bl[1]);
 }
